70cf19c45b01e18703f959a7364f73c65256579c
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-01-25 08:31:43 +0700

    up

--- testupdate.py
+++ testupdate.py
@@ -4,7 +4,7 @@ import os
 
 def check_for_updates():
     # URL đến file JSON chứa thông tin phiên bản mới nhất
-    url = "https://raw.githubusercontent.com/your_username/your_repo/main/version.json"
+    url = "https://github.com/quocanhkcn2018/update_tu/raw/main/version.json"
 
     try:
         response = requests.get(url)
@@ -15,8 +15,8 @@ def check_for_updates():
 
         # Lấy phiên bản hiện tại (bạn có thể lưu trữ trong một file cấu hình)
         current_version = 1  # Ví dụ
-
-        if latest_version > current_version:
+        print("last version " + str(latest_version))
+        if int(latest_version) > (current_version):
             print("Có phiên bản mới. Đang tải về...")
             # Tải file về
             response = requests.get(latest_download_url)
